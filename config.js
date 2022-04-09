6791a1e568a36dfed25804c918acc88f75c44b9b
Lukaszskurwysynu <69398683+Lukaszskurwysynu@users.noreply.github.com>
2022-04-09 17:38:46 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-        TOKEN: '', //write your discord bot token
+        TOKEN: 'OTYyMzcyMzY2MzcyMDQ0ODkw.YlGlHA.nj8Ls-0C1tjZU8VNpRndxkDF21Y', //write your discord bot token
         px: '!',
         playing: 'Umut Bayraktar ❤️ Music Bot',
 
